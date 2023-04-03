1518667201c28fbfabfadeed8aa9419767d8a13b
gasparllamazares <gasparelo2001@gmail.com>
2023-04-03 17:57:01 +0200

    Updated to part 1.4

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,38 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 
+// Define the main App component
 const App = () => {
+  // Define the course name and parts using an array of objects
   const course = "Half Stack application development"
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-  
-
+  // Return the App component with child components Header, Content, and Total  
   return (
     <div>
     <Header course={course} />
-    <Content
-      part1={part1.name} exercises1={part1.exercises} 
-      part2={part2.name} exercises2={part2.exercises} 
-      part3={part3.name} exercises3={part3.exercises} 
-    />
-    <Total number={part1.exercises+part2.exercises+part3.exercises} />
-    </div>
+    <Content parts={parts}/>
+    <Total parts={parts} />
+    </div> 
   )
 }
 
+  // Define the Header component with props
 const Header = (props) => {
   return(
     <div>
@@ -43,19 +41,26 @@ const Header = (props) => {
   )
 }
 
-
+  // Define the Content component with props
 const Content = (props) => {
   console.log(props)
+  console.log(props.parts)
+  props.parts.forEach(element => {
+    console.log(element.name + " " + element.exercises)
+    }
+  )
+  // Use map() to create an array of Part components for each part object in the parts array
+  // and return it wrapped in a React fragment
   return(
-    <div>
-      <Part name={props.part1} exercise={props.exercises1} />
-      <Part name={props.part2} exercise={props.exercises2} />
-      <Part name={props.part3} exercise={props.exercises3} />
-    </div>
+    <>
+    {props.parts.map(part => {
+      return <Part name={part.name} exercise={part.exercises}/>
+    })}
+    </>
   )
 }
 
-
+// Define the Part component with props
 const Part = (props) => {
   return(
     <div>
@@ -65,7 +70,7 @@ const Part = (props) => {
     </div>
   )
 }
-
+// Define the Total component with props
 const Total = (props) => {
   return(
     <div>
